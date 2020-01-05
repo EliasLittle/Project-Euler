9653c5096f35916d92c28280a6ee804515f6667c
Elias Little <eliasjlittle@gmail.com>
2020-01-04 19:33:11 -0600

    Changed to using a prime sieve and storing primes. Checking factors also stops as soon as one does not meet the criteria. Still far too slow.

--- Euler357.py
+++ Euler357.py
@@ -1,16 +1,24 @@
-import functions as f
-
+from functions import *
 
 def g(d, n):
     return d + n/d
 
-
 tot = 0
+primes = prime_sieve(1000000,output=[])
+print("Prime list of 1,000,000 found")
 for i in range(1, 10000):
-    if f.isPrime(i):
+    if i%1000== 0:
+        print(""+str(i/1000)+"% done")
+    if i in primes:
+        tot += i
         continue
-    facts = f.get_factors(i)
-    if all(f.isPrime(g(j, i)) for j in facts):
+    facts = get_factors(i,primelist=primes)
+    add = True
+    for j in facts:
+        if g(j,i) not in primes:
+            add = False
+            break
+    if add:
         tot += i
 
 print(tot)
