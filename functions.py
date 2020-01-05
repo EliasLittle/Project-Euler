16dc67f6d1ec370ff11dcbd4134e5df3f2c13439
Elias Little <eliasjlittle@gmail.com>
2020-01-04 19:06:04 -0600

    Updated many functions, and added new ones

--- functions.py
+++ functions.py
@@ -1,16 +1,21 @@
 import math
+from timeit import default_timer as timer
 
 # Returns GCD
-
-
 def gcd(a, b):
     while b != 0:
         a, b = b, a % b
     return a
 
+# Returns Eulers Totient function, the number relatively prime numbers
+def phi(n):
+    amount = 0
+    for k in range(1, n + 1):
+        if gcd(n, k) == 1:
+            amount += 1
+    return amount
+
 # Checks if a number is prime O(sqrt(n))
-
-
 def isPrime(n):
     if n <= 1:
         return False
@@ -56,7 +61,7 @@ def prime_generator(n):
 
 # get a list of all factors for N
 # ex: get_factors(10) -> [1,2,5,10]
-def get_factors(n, primelist=None):
+def get_factors(n, primelist=None,count=False):
     if primelist is None:
         primelist = prime_sieve(n, output=[])
 
@@ -71,6 +76,9 @@ def get_factors(n, primelist=None):
             n /= p
             fcount[p] += 1
 
+    if count==True:
+        return sum([f[0] for f in fcount])
+
     factors = [1]
     for i in fcount:
         level = []
@@ -81,13 +89,26 @@ def get_factors(n, primelist=None):
 
     return factors
 
+#number of prime factors
+def number_prime_factors(number):
+    i = 2
+    a = set()
+    while i < math.sqrt(number) or number == 1:
+        if number % i == 0:
+            number = number/i
+            a.add(i)
+            i -= 1
+        i += 1
+    return (len(a)+1)
 
 # get a list of prime factors
 # ex: get_prime_factors(140) -> ((2,2), (5,1), (7,1))
 #     140 = 2^2 * 5^1 * 7^1
-def get_prime_factors(n):
-    # if primelist is None:
-    primelist = prime_sieve(n, output=[])
+def get_prime_factors(n, plist=None):
+    if plist is None:
+        primelist = prime_sieve(n, output=[])
+    else:
+        primelist = plist
 
     fs = []
     for p in primelist:
@@ -106,15 +127,19 @@ def get_prime_factors(n):
 
 
 # returns whether num is a 9 digit isPandigital
-def isPandigital(num, max=9):
-    if len(str(num)) != max:
+def isPandigital(num, max=9,zero=False):
+    l = max+1 if zero else max
+    s = len(str(num))
+    if s != l:
+        #raise Exception("Length must be {}, but was {}".format(l,s))
         return False
-    for i in range(1, max+1):
+    for i in range(0 if zero else 1, max+1):
         if str(i) not in str(num):
             return False
     return True
 
 
+#Returns a list of the possible permutations
 def getPermutations(A):
     n = len(A)
     out, B = [], A
@@ -137,25 +162,66 @@ def getPermutations(A):
             i += 1
     return out
 
-"""
-Needs to be finished, currently just outlined
-def recursivePermutations(A):
-    s = set([c for c in A])
-    return ([n + recursivePermutations(s.remove(n)) for n in s])
-"""
+#Same as getPermutations, but yields them as they are found
+#Accepts array of values
+def yieldPermutations(A):
+    n = len(A)
+    B = A
+    yield tuple(A)
+    c = [0 for i in range(n)]
 
+    i = 0
+    while i < n:
+        if c[i] < i:
+            if i % 2 == 0:
+                B[0], B[i] = B[i], B[0]
+            else:
+                B[c[i]], B[i] = B[i], B[c[i]]
+            c[i] += 1
+            i = 0
+            temp = B
+            yield tuple(temp)
+        else:
+            c[i] = 0
+            i += 1
+
+# Recursive factorial funcion
 def factorial(n):
     if n <= 1:
         return 1
     else:
         return n*factorial(n-1)
 
+# Checks if n is palindromic
 def isPalindrome(n):
     s = str(n)
     if s == s[::-1]:
         return True
     return False
 
+"""
+HELPER FUNCTIONS
+"""
+
+# Concatenates all integers of a list to a single integer
+# Inverse of digits
+def toInt(A):
+    sum = 0
+    dlens = [int(math.log10(d))+1 if d > 0 else 1for d in A] # Length of each digit
+    c = 0
+    for i in range(len(A),0,-1):
+        sum += A[i-1]*math.pow(10,c)
+        #print("sum: "+str(sum))
+        c+=dlens[i-1]
+        #print("c: "+str(c))
+    return int(sum)
+
+# Returns a list of the digits of int n
+# Inverse of toInt
+def digits(n):
+    len = int(math.log10(n))+1
+    return [int((n//math.pow(10,len-i-1))) % 10 for i in range(len)]
+
 
 if __name__ == '__main__':
-    pass
+    print(toInt([1,2,3]))
